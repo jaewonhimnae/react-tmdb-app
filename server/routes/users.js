40e7c1e1fad98fea473b3cdf7152462ea38ac9aa
Huỳnh Minh Trí <trivonhan@gmail.com>
2021-03-13 16:30:19 +0700

    Fix logic bug

--- server/routes/users.js
+++ server/routes/users.js
@@ -79,7 +79,7 @@ router.post("/update", auth, (req, res) => {
         address: req.body.address,
         phoneNumber: req.body.phoneNumber
     }
-    for(let prop in params) if(!params[prop]) delete params[prop];
+    for(let prop in info) if(!info[prop]) delete info[prop];
     User.findOneAndUpdate({ _id: req.user._id }, params, (err, doc) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).send({
